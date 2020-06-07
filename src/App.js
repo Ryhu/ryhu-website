59eb6ea91d9669d9e336970e0a90b2c41e9335d2
unknown <ryanhuuu@gmail.com>
2020-06-07 16:15:34 -0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import {Link} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Error from './components/Error';
 
 function App() {
   return (
@@ -18,9 +26,28 @@ function App() {
         >
           Learn React
         </a>
+        <Row>
+          <Link to="/">Home </Link>
+          <Link to="/about">About</Link>
+          <Link to="/gallery">Gallery </Link>
+          <Link to="/contact">Contact </Link>
+        </Row>
       </header>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
+
+const Row = styled.div`
+  display: flex;
+  align-content: row;
+`
+
 
 export default App;
