13c028be1348f1559401c58f4421ce7bf8ee25fb
Ryhu <Ryanhuuu@gmail.com>
2020-06-09 14:12:06 -0400

    fixed up header

--- src/App.js
+++ src/App.js
@@ -14,34 +14,69 @@ function App() {
   return (
     <AppContainer>
       <header className="App-header">
-        <Row>
-          <Link to="/"><img id="homeHeader" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabHomeV2_nazdig.png" class="tab"></img></Link>
-          <Link to="/about">    <img id="aboutHeader" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabAboutV2_jxzxiz.png" class="tab"></img>
-</Link>
-          <Link to="/gallery">    <img id="galleryHeader" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabGalleryV2_q5tbtf.png" class="tab"></img>
-</Link>
-          <Link to="/contact">    <img id="contactHeader" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabContactV2_kj3pe1.png" class="tab"></img>
-</Link>
-        </Row>
+        <ButtonsContainer>
+          <Link to="/"><img id="homeHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabHomeV2_nazdig.png"></img></Link>
+          <div>
+            <Link to="/about"><img id="aboutHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabAboutV2_jxzxiz.png"></img></Link>
+            <Link to="/gallery"><img id="galleryHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabGalleryV2_q5tbtf.png"></img></Link>
+            <Link to="/contact"><img id="contactHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabContactV2_kj3pe1.png"></img></Link>
+          </div>
+        </ButtonsContainer>
       </header>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} />
-        <Route path="/gallery" component={Gallery} />
-        <Route path="/contact" component={Contact} />
-        <Route component={Error} />
-      </Switch>
-
-    <div id="portfolio" class="mx-auto">
+      <PortfolioCase>
+        <Page>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </Page>
+      </PortfolioCase>
+    <div id="portfolio" className="mx-auto">
       <div id="headerGap"></div>
       </div>
     </AppContainer>
   );
 }
 
-const Row = styled.div`
+const Thing = styled.div`
+  background-color: blue;
   display: flex;
-  align-content: row;
+  flex-direction: row;
+  align-items: flex-end;
+
+  a {
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  :nth-child(n) {
+    margin: 0;
+    padding: 0;
+  }
+  width: 60%;
+  background-color: black
+`
+
+const PortfolioCase = styled.div`
+  background-color: #f0d38f;
+  padding: 10px;
+  width: 65%;
+  margin: auto;
+`
+const Page = styled.div`
+  background-color: white;
+  height: 20rem;
+  width: 99%;
+  margin: auto;
 `
 const AppContainer = styled.div`
   text-align: center;
