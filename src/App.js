109ba21dc8282ab3114da4bbe44f220c4d8ff918
Ryhu <Ryanhuuu@gmail.com>
2020-06-09 17:40:20 -0400

    dumped css from codepen proj, need to redistribute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -10,35 +10,50 @@ import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 import Error from './components/Error';
 
-function App() {
-  return (
-    <AppContainer>
-      <header className="App-header">
-        <ButtonsContainer>
-          <Link to="/"><img id="homeHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabHomeV2_nazdig.png"></img></Link>
-          <div>
-            <Link to="/about"><img id="aboutHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabAboutV2_jxzxiz.png"></img></Link>
-            <Link to="/gallery"><img id="galleryHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabGalleryV2_q5tbtf.png"></img></Link>
-            <Link to="/contact"><img id="contactHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabContactV2_kj3pe1.png"></img></Link>
-          </div>
-        </ButtonsContainer>
-      </header>
-      <PortfolioCase>
-        <Page>
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/about" component={About} />
-            <Route path="/gallery" component={Gallery} />
-            <Route path="/contact" component={Contact} />
-            <Route component={Error} />
-          </Switch>
-        </Page>
-      </PortfolioCase>
-    <div id="portfolio" className="mx-auto">
-      <div id="headerGap"></div>
-      </div>
-    </AppContainer>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      class: ''
+    }
+  }
+
+  applyClass(){
+    console.log(this.state)
+    this.setState({
+      class: this.state.class == 'flip' ? '' : 'flip'
+    })
+  }
+  render(){
+    return (
+      <AppContainer>
+        <header className="App-header">
+          <ButtonsContainer>
+            <Link to="/"><img id="homeHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabHomeV2_nazdig.png"></img></Link>
+            <div>
+              <Link to="/about"><img id="aboutHeader" style={{verticalAlign: "bottom"}} src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabAboutV2_jxzxiz.png"></img></Link>
+              <Link to="/gallery"><img id="galleryHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabGalleryV2_q5tbtf.png"></img></Link>
+              <Link to="/contact"><img id="contactHeader"style={{verticalAlign: "bottom"}}  src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/Portfolio%20Resources/tabContactV2_kj3pe1.png"></img></Link>
+            </div>
+          </ButtonsContainer>
+        </header>
+        <PortfolioCase>
+          <Page className={this.state.class + " classy"}>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/about" component={About} />
+              <Route path="/gallery" component={Gallery} />
+              <Route path="/contact" component={Contact} />
+              <Route component={Error} />
+            </Switch>
+          </Page>
+        </PortfolioCase>
+        <br></br>
+      <button onClick={() => this.applyClass()}>clickety click</button>
+      </AppContainer>
+    );
+  }
 }
 
 const Thing = styled.div`
@@ -72,12 +87,14 @@ const PortfolioCase = styled.div`
   width: 65%;
   margin: auto;
   height: 43rem;
+  perspective:7000px;
 `
 const Page = styled.div`
   background-color: white;
-  height: 20rem;
+  height: 43rem;
   width: 99%;
   margin: auto;
+  transition: .5s;
 `
 const AppContainer = styled.div`
   text-align: center;
