dd2be75a4539f4f9145f31716af80a5c42271773
unknown <ryanhuuu@gmail.com>
2020-08-10 15:54:34 -0400

    Updates

---
+++ precache-manifest.cd2c786f82be81ff30f8ce9b0998a4c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e76bb65cd031e9ca3c7e958e3c5e851",
+    "url": "/ryhu-website/index.html"
+  },
+  {
+    "revision": "33ae92ee083005c2c11c",
+    "url": "/ryhu-website/static/css/main.62e9a460.chunk.css"
+  },
+  {
+    "revision": "d342ac45bf0686a8e993",
+    "url": "/ryhu-website/static/js/2.fdd982f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ryhu-website/static/js/2.fdd982f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ae92ee083005c2c11c",
+    "url": "/ryhu-website/static/js/main.bccb6c3c.chunk.js"
+  },
+  {
+    "revision": "853b2aa3e9b1fdd952d7",
+    "url": "/ryhu-website/static/js/runtime-main.0553551a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ryhu-website/static/media/logo.ee7cd8ed.svg"
+  }
+]);
