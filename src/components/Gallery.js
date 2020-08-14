ab233114e87775f5b6c367ea39238cecddcab73d
Ryhu <Ryanhuuu@gmail.com>
2020-08-13 21:30:03 -0400

    Standardized displays for gallery, looks MUCH better now

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,38 +9,51 @@ function Gallery() {
       <ProjectRow>
         <Tooltip placement="bottom" tooltip={SimonDescription}>
           <Thumbnail href="https://codepen.io/Ryhu/full/eGeXbv/" target="_blank">
-            <img class="pic" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/SimonMini_ybnppw.png"></img>
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/SimonMini_ybnppw.png"></ThumbnailImage>
           </Thumbnail>
         </Tooltip>
-        <Thumbnail href="https://codepen.io/Ryhu/full/ZJgvMv/" target="_blank">
-          <img class="pic" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/PomodoroMini_ka4sfc.png"></img>
-        </Thumbnail>
-        <div>
-          Pomodoro timer for productivity, capable of switching between break and session timers
-        </div>
+        <Tooltip placement="bottom" tooltip={PomodoroDescription}>
+          <Thumbnail href="https://codepen.io/Ryhu/full/ZJgvMv/" target="_blank">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/PomodoroMini_ka4sfc.png"></ThumbnailImage>
+          </Thumbnail>
+        </Tooltip>
       </ProjectRow>
       <ProjectRow>
-        <Thumbnail href="https://codepen.io/Ryhu/full/zwmbgp/" target="_blank">
-          <img class="pic" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/CalculatorMini_xw27y0.png"></img>
-        </Thumbnail>
-        <div>
-          Fully functional calculator with a 7 digit display
-        </div>
-        <Thumbnail href="https://codepen.io/Ryhu/full/yXdNwE/" target="_blank">
-          <img class="pic" src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/TicTacToeMini_pp4uvj.png"></img>
-        </Thumbnail>
-        <div>
-          Tic-Tac-Toe game, in either single or multi-player mode
-        </div>
+        <Tooltip placement="top" tooltip={CalculatorDescription}>
+          <Thumbnail href="https://codepen.io/Ryhu/full/zwmbgp/" target="_blank">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/CalculatorMini_xw27y0.png"></ThumbnailImage>
+          </Thumbnail>
+        </Tooltip>
+        <Tooltip placement="top" tooltip={TicTacToeDescription}>
+          <Thumbnail href="https://codepen.io/Ryhu/full/yXdNwE/" target="_blank">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/TicTacToeMini_pp4uvj.png"></ThumbnailImage>
+          </Thumbnail>
+        </Tooltip>
       </ProjectRow>
     </GalleryContainer>
   )
 };
 
+// style={{transform: "rotate(2deg)"}}
+
+const ThumbnailImage = styled.img`
+  box-shadow: grey 5px 5px 3px;
+`
+
 const DescriptionPopperTitle = styled.div`
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: .5rem;
+  margin-top: -.5rem;
+
+`
+
+const DescriptionPopperText = styled.div`
 `
 
 const DescriptionPopper = styled.div`
+  padding: 1rem;
 `
 
 const GalleryContainer = styled.div`
@@ -51,6 +64,7 @@ const GalleryContainer = styled.div`
 `
 
 const Thumbnail = styled.a`
+  height: 100%;
 `
 
 const ProjectRow = styled.div`
@@ -62,11 +76,41 @@ const ProjectRow = styled.div`
 const SimonDescription = (
   <DescriptionPopper>
     <DescriptionPopperTitle>
-      Digital version of popular game "Simon", with a hard mode modifier
+      Simon
     </DescriptionPopperTitle>
+    <DescriptionPopperText>
+      Digital version of popular game "Simon", with a hard mode modifier
+    </DescriptionPopperText>
 
   </DescriptionPopper>
 );
+
+const PomodoroDescription = (
+  <DescriptionPopper>
+    <DescriptionPopperTitle>
+    Pomodoro timer for productivity, capable of switching between break and session timers
+    </DescriptionPopperTitle>
+  </DescriptionPopper>
+);
+
+
+const CalculatorDescription = (
+  <DescriptionPopper>
+    <DescriptionPopperTitle>
+      Fully functional calculator with a 7 digit display
+    </DescriptionPopperTitle>
+  </DescriptionPopper>
+);
+
+
+const TicTacToeDescription = (
+  <DescriptionPopper>
+    <DescriptionPopperTitle>
+      Tic-Tac-Toe game, in either single or multi-player mode
+    </DescriptionPopperTitle>
+  </DescriptionPopper>
+);
+
 
 
 
