8a83b269711ddbe2ddf5c446ffa6a063298aa5c8
Ryhu <Ryanhuuu@gmail.com>
2020-08-26 21:32:40 -0400

    added words to about, in comments

--- src/components/About.js
+++ src/components/About.js
@@ -3,23 +3,57 @@ import React from "react"
 function About() {
   return (
     <div>
-      <div>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Ryan Hu
 
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      New york, new york
 
-
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-Where can I get some?
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-      </div>
     </div>
   )
 }
+
+
+//goals: 
+// short summary
+
+// skills
+// explaination of skills
+
+// angular
+/*
+The tech stack at my previous job was Angular, on ruby on rails. 
+My experience with Angular mainly consisted of building an administrative website for our app.
+this involved mainly building functionality for the website: various inputs for users to add new data, and various pages to display the new data. 
+
+Over my time at Altru, I've touched on many technologies, such as route-guards, rxjs, and redux to name a few. 
+I've also added testing to the angular app using Karma + Jasmine.
+
+
+// ruby on rails
+My experience with RoR comes from both my previous job and my time at Flatiron.
+During my time at Altru Labs,  I was in charge of adding features to the application, so whatever I did on the front end, I had to support on the back end.
+This involved making new models, data migrations, writing tests with rspecF, basic backend stuff, with an emphesis on writing tests, as every feature i added into the backend was thouroughly tested.
+Apart that, I also worked on an async server, supported by rabbitMQ. Here, I worked on email processing, video processing, and translations. 
+
+// react
+My experience with react stems from both my time at the bootcamp, and my personal projects. In fact, this website is built on react. 
+My experience with react should be shown, not told: just go over to the gallery and explore a couple of my projects!
+*/
+
+
+// Ryan Hu
+// I'm a software engineer based in New York, speciailizing in building websites.
+/* 
+my love of code stems from a love of automation; my first coding language was neither java or javascript, but autohotkey (C++?).
+At a certain point in my life, I was working in a data entry position. I had to take a (quite literal) room of files, and process them into our legal software. 
+After realizing that the files were available online, I made the gamble that I could probably automate the process so my next few weeks would be much less tedious.
+Writing the macro was no easy work, as there were many variables to consider: download times werent consistant, the computer might lag, and if anything went wrong, I'd need a way to verify if a file was stored properly or not. 
+It was a similiar process to coding, I spent 20% of the time writing the actual thing, and 80% debugging, watching it, and making improvements.
+A week later, after we sent a room full of paper to a shredding service, I realized that I'd pefer this kind of work to whatever i was doing at the time, and applied for a bootcamp.
+
+
+*/
+// 
+// https://medium.com/@cgroom/a-software-engineers-one-page-portfolio-4f85ab8a20d1
+
 
 export default About
