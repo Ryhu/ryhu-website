e488ae64f37d79f5bdd64cd0ebc63a301e587c90
Ryhu <Ryanhuuu@gmail.com>
2020-08-30 20:12:39 -0400

    cleaned up gallery code a little, added slight delay to tooltip

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,24 +12,24 @@ function Gallery() {
       </div>
       <GalleryImages>
         <ProjectRow>
-          <Tooltip placement="bottom" tooltip={SimonDescription}>
+          <Tooltip placement="bottom" tooltip={SimonDescription} delayShow='150'>
             <Thumbnail href="https://codepen.io/Ryhu/full/eGeXbv/" target="_blank">
               <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/SimonMini_ybnppw.png"></ThumbnailImage>
             </Thumbnail>
           </Tooltip>
-          <Tooltip placement="bottom" tooltip={PomodoroDescription}>
+          <Tooltip placement="bottom" tooltip={PomodoroDescription} delayShow='150'>
             <Thumbnail href="https://codepen.io/Ryhu/full/ZJgvMv/" target="_blank">
               <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/PomodoroMini_ka4sfc.png"></ThumbnailImage>
             </Thumbnail>
           </Tooltip>
         </ProjectRow>
         <ProjectRow>
-          <Tooltip placement="top" tooltip={CalculatorDescription}>
+          <Tooltip placement="top" tooltip={CalculatorDescription} delayShow='150'>
             <Thumbnail href="https://codepen.io/Ryhu/full/zwmbgp/" target="_blank">
               <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/CalculatorMini_xw27y0.png"></ThumbnailImage>
             </Thumbnail>
           </Tooltip>
-          <Tooltip placement="top" tooltip={TicTacToeDescription}>
+          <Tooltip placement="top" tooltip={TicTacToeDescription} delayShow='150'>
             <Thumbnail href="https://codepen.io/Ryhu/full/yXdNwE/" target="_blank">
               <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/TicTacToeMini_pp4uvj.png"></ThumbnailImage>
             </Thumbnail>
@@ -56,14 +56,11 @@ const DescriptionPopperTitle = styled.div`
   font-size: 1.2rem;
   margin-bottom: .5rem;
   margin-top: -.5rem;
-
-`
-
-const DescriptionPopperText = styled.div`
 `
 
 const DescriptionPopper = styled.div`
   padding: 1rem;
+  padding-bottom: 0;
   max-width: 26rem;
 `
 
@@ -78,7 +75,6 @@ const GalleryImages = styled.div`
   height: 100%;
 `
 
-
 const Thumbnail = styled.a`
   height: 100%;
 `
@@ -88,25 +84,21 @@ const ProjectRow = styled.div`
   flex-direction: row;
   justify-content: space-around;
   margin: 3rem;
+
+  .tooltip-container{
+    border-width: 1rem;
+  }
 `
-  
+
 const SimonDescription = (
   <DescriptionPopper>
     <DescriptionPopperTitle>
       Simon
     </DescriptionPopperTitle>
-    <DescriptionPopperText>
-      A digital version of popular game "Simon". 
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      The game lights up colors in a sequence, and the player repeats the sequence to the machine, winning after 20 rounds of progressively harder sequences.
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      Game can be turned on or off at will, and includes an option to turn on "strict mode", where one error will return the player back to the first step, instead of restarting the current step.
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      Also makes beep-boop noises if you're into that kind of thing.
-    </DescriptionPopperText>
+    <p>A digital version of popular game "Simon". </p>
+    <p>The game lights up colors in a sequence, and the player repeats the sequence to the machine, winning after 20 rounds of progressively harder sequences.</p>
+    <p>Game can be turned on or off at will, and includes an option to turn on "strict mode", where one error will return the player back to the first step, instead of restarting the current step.</p>
+    <p>Also makes beep-boop noises if you're into that kind of thing.</p>
   </DescriptionPopper>
 );
 
@@ -115,15 +107,9 @@ const PomodoroDescription = (
     <DescriptionPopperTitle>
       Pomodoro Clock
     </DescriptionPopperTitle>
-    <DescriptionPopperText>
-      A Pomodoro(Italian for tomato) timer for productivity, switching between periods of work and rest, based on the Pomodoro method.
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      By default, switches between 25 minutes of work, and 5 minutes of rest, but that can be customized using the easy-to-use interface. 
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      Does not resemble a tomato in any way.
-    </DescriptionPopperText>
+    <p>A Pomodoro(Italian for tomato) timer for productivity, switching between periods of work and rest, based on the Pomodoro method.</p>
+    <p>By default, switches between 25 minutes of work, and 5 minutes of rest, but that can be customized using the easy-to-use interface. </p>
+    <p>Does not resemble a tomato in any way.</p>
   </DescriptionPopper>
 );
 
@@ -132,12 +118,8 @@ const CalculatorDescription = (
     <DescriptionPopperTitle>
       Calculator
     </DescriptionPopperTitle>
-    <DescriptionPopperText>
-      A fully functional calculator modeled after one of those old-timey desktop calculators(Do people still use those?)
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      All the classic functions are there, addition, subtraction, multiplication, division, it's even got that new-fangled back button!
-    </DescriptionPopperText>
+    <p>A fully functional calculator modeled after one of those old-timey desktop calculators(Do people still use those?)</p>
+    <p>All the classic functions are there, addition, subtraction, multiplication, division, it's even got that new-fangled back button!</p>
   </DescriptionPopper>
 );
 
@@ -146,18 +128,10 @@ const TicTacToeDescription = (
     <DescriptionPopperTitle>
       Tic-Tac-Toe
     </DescriptionPopperTitle>
-    <DescriptionPopperText>
-      A typical Tic-Tac-Toe game, done in JS and JQuery. 
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      Playable in either single or two player modes, against either an AI or another human player.
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      The AI isnt perfect, is programmed to make mistakes from time to time to allow player to win. 
-    </DescriptionPopperText>
-    <DescriptionPopperText>
-      Will definitely stick to an animation library the next time I make something like this. 
-    </DescriptionPopperText>
+    <p> A typical Tic-Tac-Toe game, done in JS and JQuery. </p>
+    <p> Playable in either single or two player modes, against either an AI or another human player.</p>
+    <p> The AI isnt perfect, is programmed to make mistakes from time to time to allow player to win. </p>
+    <p> Will definitely stick to an animation library the next time I make something like this. </p>
   </DescriptionPopper>
 );
 
