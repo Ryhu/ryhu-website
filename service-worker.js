0b5d02845df433daf5da383a37e461d836cddebe
Ryhu <Ryanhuuu@gmail.com>
2020-08-30 20:48:26 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ryhu-website/precache-manifest.d42dabfccdcf0f4e519c4cc1b41b13c1.js"
+  "/ryhu-website/precache-manifest.d5cb95e8026d8be265f696b7a4925fe3.js"
 );
 
 self.addEventListener('message', (event) => {
