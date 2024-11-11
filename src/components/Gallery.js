8ebf6c637eda30dbf96210ef6b511c443de733ff
Ryan <ryanhuuu@gmail.com>
2024-11-11 15:47:01 -0500

    linting

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,58 +1,98 @@
-import React from "react"
-import styled from 'styled-components'
-import TooltipTrigger from 'react-popper-tooltip';
-import 'react-popper-tooltip/dist/styles.css';
+import React from "react";
+import styled from "styled-components";
+import TooltipTrigger from "react-popper-tooltip";
+import "react-popper-tooltip/dist/styles.css";
 
 function Gallery() {
-
   const tooltipHelper = (projectName) => {
     return (
       <DescriptionPopper>
-      <DescriptionPopperTitle>
-        {popperContent[projectName].title}
-      </DescriptionPopperTitle>
+        <DescriptionPopperTitle>
+          {popperContent[projectName].title}
+        </DescriptionPopperTitle>
         {popperContent[projectName].content}
       </DescriptionPopper>
-    )
-  }
+    );
+  };
 
   const popperContent = {
     simon: {
-      title: 'Simon',
-      content: <>
-        <p>A digital version of popular game "Simon". </p>
-        <p>The game lights up colors in a sequence, and the player repeats the sequence to the machine, winning after 20 rounds of progressively harder sequences.</p>
-        <p>Game can be turned on or off at will, and includes an option to turn on "strict mode", where one error will return the player back to the first step, instead of restarting the current step.</p>
-        <p>Also makes beep-boop noises if you're into that kind of thing.</p>
-      </>
+      title: "Simon",
+      content: (
+        <>
+          <p>A digital version of popular game "Simon". </p>
+          <p>
+            The game lights up colors in a sequence, and the player repeats the
+            sequence to the machine, winning after 20 rounds of progressively
+            harder sequences.
+          </p>
+          <p>
+            Game can be turned on or off at will, and includes an option to turn
+            on "strict mode", where one error will return the player back to the
+            first step, instead of restarting the current step.
+          </p>
+          <p>Also makes beep-boop noises if you're into that kind of thing.</p>
+        </>
+      ),
     },
     pomodoro: {
-      title: 'Pomodoro Clock',
-      content: <>
-        <p>A Pomodoro(Italian for tomato) timer for productivity, switching between periods of work and rest, based on the Pomodoro method.</p>
-        <p>By default, switches between 25 minutes of work, and 5 minutes of rest, but that can be customized using the easy-to-use interface. </p>
-        <p>Does not resemble a tomato in any way.</p>
-      </>
+      title: "Pomodoro Clock",
+      content: (
+        <>
+          <p>
+            A Pomodoro(Italian for tomato) timer for productivity, switching
+            between periods of work and rest, based on the Pomodoro method.
+          </p>
+          <p>
+            By default, switches between 25 minutes of work, and 5 minutes of
+            rest, but that can be customized using the easy-to-use interface.{" "}
+          </p>
+          <p>Does not resemble a tomato in any way.</p>
+        </>
+      ),
     },
     calculator: {
-      title: 'Calculator',
-      content: <>
-        <p>A fully functional calculator modeled after one of those old-timey desktop calculators(Do people still use those?)</p>
-        <p>All the classic functions are there, addition, subtraction, multiplication, division, it's even got that new-fangled back button!</p>
-      </>
+      title: "Calculator",
+      content: (
+        <>
+          <p>
+            A fully functional calculator modeled after one of those old-timey
+            desktop calculators(Do people still use those?)
+          </p>
+          <p>
+            All the classic functions are there, addition, subtraction,
+            multiplication, division, it's even got that new-fangled back
+            button!
+          </p>
+        </>
+      ),
     },
     ticTacToe: {
-      title: 'Tic-Tac-Toe',
-      content: <>
-        <p> A typical Tic-Tac-Toe game, done in JS and JQuery. </p>
-        <p> Playable in either single or two player modes, against either an AI or another human player.</p>
-        <p> The AI isnt perfect, is programmed to make mistakes from time to time to allow player to win. </p>
-        <p> Will definitely stick to an animation library the next time I make something like this. </p>
-      </>
+      title: "Tic-Tac-Toe",
+      content: (
+        <>
+          <p> A typical Tic-Tac-Toe game, done in JS and JQuery. </p>
+          <p>
+            {" "}
+            Playable in either single or two player modes, against either an AI
+            or another human player.
+          </p>
+          <p>
+            {" "}
+            The AI isnt perfect, is programmed to make mistakes from time to
+            time to allow player to win.{" "}
+          </p>
+          <p>
+            {" "}
+            Will definitely stick to an animation library the next time I make
+            something like this.{" "}
+          </p>
+        </>
+      ),
     },
   };
 
-  const Tooltip = ({children, tooltip, hideArrow, ...props}) => (
+  const Tooltip = ({ children, tooltip, hideArrow, ...props }) => (
     <TooltipTrigger
       {...props}
       tooltip={({
@@ -60,34 +100,31 @@ function Gallery() {
         tooltipRef,
         getArrowProps,
         getTooltipProps,
-        placement
+        placement,
       }) => (
         <div
           {...getTooltipProps({
             ref: tooltipRef,
-            className: 'tooltip-container',
-          })}
-        >
+            className: "tooltip-container",
+          })}>
           {!hideArrow && (
             <div
               {...getArrowProps({
                 ref: arrowRef,
-                className: 'tooltip-arrow',
-                'data-placement': placement
+                className: "tooltip-arrow",
+                "data-placement": placement,
               })}
             />
           )}
           {tooltip}
         </div>
-      )}
-    >
-      {({getTriggerProps, triggerRef}) => (
+      )}>
+      {({ getTriggerProps, triggerRef }) => (
         <span
           {...getTriggerProps({
             ref: triggerRef,
-            className: 'trigger'
-          })}
-        >
+            className: "trigger",
+          })}>
           {children}
         </span>
       )}
@@ -95,36 +132,50 @@ function Gallery() {
   );
 
   return (
-      <>
-        <GalleryHeader>
-          <p>Gallery</p>
-          <p>Mouse over the images to get more details, click to see a live demo</p>
-        </GalleryHeader>
-        <GalleryContent>
-          <a href="https://codepen.io/Ryhu/full/eGeXbv/" target="_blank">
-            <Tooltip placement="bottom" tooltip={tooltipHelper('simon')} delayShow='150'>
-              <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/SimonMini_ybnppw.png"></ThumbnailImage>
-            </Tooltip>
-          </a>
-          <a href="https://codepen.io/Ryhu/full/ZJgvMv/" target="_blank">
-            <Tooltip placement="bottom" tooltip={tooltipHelper('pomodoro')} delayShow='150'>
-              <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/PomodoroMini_ka4sfc.png"></ThumbnailImage>
-            </Tooltip>
-          </a>
-          <a href="https://codepen.io/Ryhu/full/zwmbgp/" target="_blank">
-            <Tooltip placement="top" tooltip={tooltipHelper('calculator')} delayShow='150'>
-              <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/CalculatorMini_xw27y0.png"></ThumbnailImage>
-            </Tooltip>
-          </a>
-          <a href="https://codepen.io/Ryhu/full/yXdNwE/" target="_blank">
-            <Tooltip placement="top" tooltip={tooltipHelper('ticTacToe')} delayShow='150'>
-              <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/TicTacToeMini_pp4uvj.png"></ThumbnailImage>
-            </Tooltip>
-          </a>
-        </GalleryContent>
-      </>
-  )
-};
+    <>
+      <GalleryHeader>
+        <p>Gallery</p>
+        <p>
+          Mouse over the images to get more details, click to see a live demo
+        </p>
+      </GalleryHeader>
+      <GalleryContent>
+        <a href="https://codepen.io/Ryhu/full/eGeXbv/" target="_blank">
+          <Tooltip
+            placement="bottom"
+            tooltip={tooltipHelper("simon")}
+            delayShow="150">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/SimonMini_ybnppw.png"></ThumbnailImage>
+          </Tooltip>
+        </a>
+        <a href="https://codepen.io/Ryhu/full/ZJgvMv/" target="_blank">
+          <Tooltip
+            placement="bottom"
+            tooltip={tooltipHelper("pomodoro")}
+            delayShow="150">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/PomodoroMini_ka4sfc.png"></ThumbnailImage>
+          </Tooltip>
+        </a>
+        <a href="https://codepen.io/Ryhu/full/zwmbgp/" target="_blank">
+          <Tooltip
+            placement="top"
+            tooltip={tooltipHelper("calculator")}
+            delayShow="150">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/CalculatorMini_xw27y0.png"></ThumbnailImage>
+          </Tooltip>
+        </a>
+        <a href="https://codepen.io/Ryhu/full/yXdNwE/" target="_blank">
+          <Tooltip
+            placement="top"
+            tooltip={tooltipHelper("ticTacToe")}
+            delayShow="150">
+            <ThumbnailImage src="http://res.cloudinary.com/devvqi6h0/image/upload/v1512596651/minis/TicTacToeMini_pp4uvj.png"></ThumbnailImage>
+          </Tooltip>
+        </a>
+      </GalleryContent>
+    </>
+  );
+}
 
 const GalleryHeader = styled.div`
   p {
@@ -133,24 +184,23 @@ const GalleryHeader = styled.div`
   }
 
   p:first-child {
-    margin-top: .7rem;
+    margin-top: 0.7rem;
     margin-bottom: 0rem;
     font-size: 1.5rem;
     font-weight: bold;
   }
-`
+`;
 
 const ThumbnailImage = styled.img`
-
-  //portrait 
-  @media screen and (max-width: 640px) and (max-height: 950px){
+  //portrait
+  @media screen and (max-width: 640px) and (max-height: 950px) {
     height: 15vh;
     margin-left: 20vw;
     margin-right: 20vw;
-    margin-bottom: .5rem;
+    margin-bottom: 0.5rem;
   }
 
-  //landscape 
+  //landscape
   @media screen and (max-height: 640px) and (max-width: 950px) {
     width: 20vw;
   }
@@ -159,25 +209,25 @@ const ThumbnailImage = styled.img`
   @media screen and (min-width: 950px) {
     margin-left: 4rem;
     margin-right: 4rem;
-    margin-bottom: 2rem; 
+    margin-bottom: 2rem;
     width: 19rem;
   }
 
   box-shadow: grey 5px 5px 3px;
-`
+`;
 
 const DescriptionPopperTitle = styled.div`
   font-weight: bold;
   font-size: 1.2rem;
-  margin-bottom: .5rem;
-  margin-top: -.5rem;
-`
+  margin-bottom: 0.5rem;
+  margin-top: -0.5rem;
+`;
 
 const DescriptionPopper = styled.div`
   padding: 1rem;
   padding-bottom: 0;
   max-width: 26rem;
-`
+`;
 
 const GalleryContent = styled.div`
   display: flex;
@@ -189,6 +239,6 @@ const GalleryContent = styled.div`
   @media screen and (max-height: 640px) {
     margin-top: 10vh;
   }
-`
+`;
 
-export default Gallery
+export default Gallery;
