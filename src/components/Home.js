6be461e4c090c8caf2d13758c0ae8f93825f0517
Ryan <ryanhuuu@gmail.com>
2025-03-11 17:51:07 -0400

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,45 @@
-import React from "react"
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function Home() {
-  return(
+  return (
     <>
-      <HomeHeaderGap src="https://res.cloudinary.com/devvqi6h0/image/upload/v1591644755/portfolio%20resources/tabGap2SeeThrough.png"></HomeHeaderGap>
-      <HomeBackground>
-        <TitleText >Hu, Ryan</TitleText>
-        <p>website currently under construction, please excuse the mess!</p>
-      </HomeBackground>
+      <HomeWrapper>
+        <HomeHeaderGap src="https://res.cloudinary.com/devvqi6h0/image/upload/v1591644755/portfolio%20resources/tabGap2SeeThrough.png"></HomeHeaderGap>
+        <HomeContent>
+          <TitleText>Hu, Ryan</TitleText>
+          <p>website currently under construction, please excuse the mess!</p>
+        </HomeContent>
+      </HomeWrapper>
     </>
-  )
+  );
 }
 
-const HomeHeaderGap = styled.img`
-  max-width: 80%;
-  vertical-align: bottom;
-`
-
-const HomeBackground = styled.div`
+const HomeWrapper = styled.div`
   background-color: #ffdead;
   height: 100%;
-  display: flex;
   flex-direction: column;
+  justify-content: space-between;
+`;
+
+const HomeHeaderGap = styled.img`
+  width: 100%;
+  vertical-align: bottom;
+`;
+
+const HomeContent = styled.div`
+  // TODO: fix this
+  margin-top: 25vh;
+  position: relative;
+  background-color: #ffdead;
   justify-content: center;
-`
+`;
 
 const TitleText = styled.div`
+  // animation: 1s ease-out example;
+
   background-color: #ffdead;
   font-size: 6rem;
-`
+`;
 
-export default Home
+export default Home;
