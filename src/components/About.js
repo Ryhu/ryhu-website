b8ce0f4c172a0888f9af0479a2b46fe440dcadab
Ryan <ryanhuuu@gmail.com>
2025-03-11 17:53:22 -0400

    WIP: got a bouncy folder for about, persist through page changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faReact } from '@fortawesome/free-brands-svg-icons'
-import { faGem, faFileAlt, faHeart } from '@fortawesome/free-solid-svg-icons'
-import TooltipTrigger from 'react-popper-tooltip';
-import 'react-popper-tooltip/dist/styles.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngular, faReact } from "@fortawesome/free-brands-svg-icons";
+import { faGem, faFileAlt, faHeart } from "@fortawesome/free-solid-svg-icons";
+import TooltipTrigger from "react-popper-tooltip";
+import "react-popper-tooltip/dist/styles.css";
 
-function About() {
+function About({ status }) {
   const [tab, setTab] = useState("summary");
 
   return (
     <div>
-
       <div>
         <AboutHeader> Ryan Hu</AboutHeader>
       </div>
+      <breakout>
+        <PageWrapper
+          className="samplePage"
+          onClick={() => {
+            alert("hihi");
+          }}>
+          <NavButtonImage
+            draggable="false"
+            onClick={() => {
+              alert("hihi");
+            }}
+            src="https://res.cloudinary.com/devvqi6h0/image/upload/v1597780834/portfolio%20resources/portfolioTabsGallery.png"></NavButtonImage>
+          <Page>hihi</Page>
+        </PageWrapper>
+      </breakout>
 
+      {/* 
       <IconRow>
         <Tooltip placement="bottom" tooltip='Summary'><div className={ "iconButton " + (tab === 'summary' ? 'selected' : '') } onClick={() => {setTab('summary')}}> <FontAwesomeIcon icon={faFileAlt} fixedWidth /> </div></Tooltip>
         <Tooltip placement="bottom" tooltip='Why I code'><div className={ "iconButton " + (tab === 'heart' ? 'selected' : '') } onClick={() => {setTab('heart')}}> <FontAwesomeIcon icon={faHeart} fixedWidth /> </div></Tooltip>
@@ -60,13 +75,154 @@ function About() {
           This involved making new models, data migrations, writing tests with Rspec, basic backend stuff, with an emphasis on writing tests, as every feature I added into the backend was thoroughly tested.
           Apart that, I also worked on an async server, supported by RabbitMQ. Here, I worked on email processing, video processing, and translations. 
         </StoryContainer>
-      </AboutTabContentContainer>
+      </AboutTabContentContainer> */}
 
-      {/* 
-      */}
+      {/*
+       */}
     </div>
-  )
+  );
 }
+
+const breakout = styled.div`
+  position: relative;
+`;
+
+const PageWrapper = styled.div`
+  &:hover {
+    // animation: hoverToHover 1s forward;
+
+    // animation: hoverToHover 1s ease-in-out 3s forwards;
+  }
+
+  // transition: 0.5s ease-in-out;
+  transition: 0.3s cubic-bezier(0.42, 0, 0.13, 1.89);
+
+  &:hover {
+    transform: translateY(-30px);
+    // animation: hoverToHover 1s ease-in-out forwards;
+  }
+  // button{
+  //   animation: rotate360 1.2s linear infinite;  /* animation set */
+  //   animation-play-state: paused;               /* ...but paused */
+  // }
+  // button:hover{
+  //   animation-play-state: running;              /* Run on hover! */
+  // }
+  // @keyframes rotate360 {
+  //   to { transform: rotate(360deg); }
+  // }
+
+  width: 800px;
+  position: relative;
+
+  // animation: mySpinner 4s;
+
+  @keyframes hoverToHover {
+    0% {
+    }
+
+    100% {
+      transform: translateY(-30px);
+    }
+  }
+
+  @keyframes mySpinner {
+    0% {
+      transform-style: preserve-3d;
+      position: relative;
+      // z-index: 999; !important;
+    }
+
+    50% {
+      transform-style: preserve-3d;
+      position: relative;
+      // z-index: 999; !important;
+      transform: translateY(-500px);
+    }
+
+    100% {
+      transform-style: preserve-3d;
+      left: 0px;
+      top: 0px;
+    }
+  }
+`;
+
+// const PageWrapper = styled.div`
+//   width: 800px;
+
+//   animation: mySpinner 4s;
+
+//   @keyframes mySpinner {
+//     0% {
+//       position: absolute;
+//       background: red;
+//       left: 0px;
+//       top: 0px;
+//     }
+//     25% {
+//       position: absolute;
+//       background: yellow;
+//       left: 200px;
+//       top: 0px;
+//     }
+//     50% {
+//       position: absolute;
+//       background: blue;
+//       left: 200px;
+//       top: 200px;
+//     }
+//     75% {
+//       position: absolute;
+//       background: green;
+//       left: 0px;
+//       top: 200px;
+//     }
+
+//     95% {
+//       position: absolute;
+//       background: green;
+//       left: 0px;
+//       top: 0px;
+//     }
+//     100% {
+//       background: red;
+//       left: 0px;
+//       top: 0px;
+//     }
+//   }
+// `;
+
+const NavButtonImage = styled.img`
+  // TODO: HARD CODED
+
+  width: 70px;
+  @media (min-width: 600px) {
+    width: 120px;
+  }
+  @media (min-width: 961px) {
+    width: 150px;
+  }
+
+  vertical-align: bottom;
+`;
+
+const Page = styled.div`
+  background-color: blue;
+  // height: -webkit-fill-available;
+  // height: calc(100% - 40px) !important;
+  height: 400px !important;
+  width: 800px;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  transition: transform 0.5s, opacity 0.5s;
+  transform-origin: bottom;
+  overflow: hidden;
+
+  // @media (min-width:600px) {
+  //   height: calc(100% - 44px);
+  // }
+`;
 
 const IconRow = styled.div`
   background-color: #1cabe3;
@@ -75,29 +231,29 @@ const IconRow = styled.div`
   box-shadow: 2px 2px 5px;
   padding: 0.5rem;
 
-  .iconButton{
+  .iconButton {
     display: inline-block;
     border-radius: 5rem;
     cursor: pointer;
     background-color: #f2f2f2;
-    margin: .3rem;
-    
-    &.selected{
+    margin: 0.3rem;
+
+    &.selected {
       background-color: blue;
       color: white;
     }
 
-    :hover{
+    :hover {
       background-color: blue;
       color: white;
     }
   }
 
-  svg{
+  svg {
     font-size: 3rem;
     padding: 1rem;
   }
-`
+`;
 
 const AboutTabContentContainer = styled.div`
   width: 80%;
@@ -106,31 +262,31 @@ const AboutTabContentContainer = styled.div`
   box-shadow: 2px 2px 5px;
   text-align: left;
 
-  div{
+  div {
     margin: auto;
 
-    .title{
+    .title {
       margin-top: -1.8rem;
       font-weight: bold;
       font-size: 1.7rem;
     }
   }
-`
+`;
 
 const StoryContainer = styled.div`
   padding: 3rem;
   font-size: 1.3rem;
   margin-top: 1rem;
   background-color: #f7ed8e;
-`
+`;
 
 const AboutHeader = styled.div`
   font-size: 3rem;
   font-weight: bold;
   margin-bottom: 1rem;
-`
+`;
 
-const Tooltip = ({children, tooltip, hideArrow, ...props}) => (
+const Tooltip = ({ children, tooltip, hideArrow, ...props }) => (
   <TooltipTrigger
     {...props}
     tooltip={({
@@ -138,29 +294,26 @@ const Tooltip = ({children, tooltip, hideArrow, ...props}) => (
       tooltipRef,
       getArrowProps,
       getTooltipProps,
-      placement
+      placement,
     }) => (
       <div
         {...getTooltipProps({
           ref: tooltipRef,
-          className: 'tooltip-container',
-        })}
-      >
+          className: "tooltip-container",
+        })}>
         {tooltip}
       </div>
-    )}
-  >
-    {({getTriggerProps, triggerRef}) => (
+    )}>
+    {({ getTriggerProps, triggerRef }) => (
       <span
         {...getTriggerProps({
           ref: triggerRef,
-          className: 'trigger'
-        })}
-      >
+          className: "trigger",
+        })}>
         {children}
       </span>
     )}
   </TooltipTrigger>
 );
 
-export default About
+export default About;
